4ca4e4045d9de86335493cd345938b71a809b3b0
makerswr <alceoswr@gmail.com>
2022-06-20 14:18:14 +0000

    logout func

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,16 +1,23 @@
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "./Navigation";
 
 const AppRouter = ({ isLoggedIn }) => {
 	return (
 		<Router>
+			{isLoggedIn && <Navigation />}
 			<Routes>
 				{isLoggedIn ? (
-					<Route path="/" element={<Home />} />
+					<>	
+						<Route path="/" element={<Home />} />
+						<Route path="/profile" element={<Profile />} />
+					</>
 				) : (
 					<Route path="/" element={<Auth />} />
 				)}
+				
 			</Routes>
 		</Router>
 	);
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -3,6 +3,9 @@ import { useState } from "react";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+  GithubAuthProvider,		
 } from "firebase/auth";
 
 const Auth = () => {
@@ -47,6 +50,21 @@ const Auth = () => {
 
 			
 	const toggleAccount = () => setNewAccount((prev) => !prev);
+	
+	const onSocialClick = async (event) => {
+		const {
+			target: { name },
+		} = event;
+		let provider;
+		if (name === "google") {
+			provider = new GoogleAuthProvider();
+		} else if (name === "github") {
+			provider = new GithubAuthProvider();
+		}
+		const data = await signInWithPopup(authService, provider);
+		console.log(data)
+	};
+	
 	return (
 	<div>
 		<form onSubmit={onSubmit}>
@@ -59,8 +77,12 @@ const Auth = () => {
 			{newAccount ? "Sign In" : "Create Account"}
 		</span>	
 		<div>
-			<button>Continue with Google</button>
-			<button>Continue with Github</button>
+			<button onClick={onSocialClick} name="google">
+				Continue with Google
+			</button>
+			<button onClick={onSocialClick} name="github">
+				Continue with Github
+			</button>
 		</div>
 	</div>
 	);
