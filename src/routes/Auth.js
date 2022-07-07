a9ebeb3b546d87a9ecbad8140c7fc9c0fe1348b8
makerswr <alceoswr@gmail.com>
2022-07-07 13:53:55 +0000

    all func implemented

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,5 @@
-import { authService } from "fbase";
-import { useState } from "react";
+import { authService, firebaseInstance } from "fbase";
+import AuthForm from "components/AuthForm";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
@@ -9,47 +9,6 @@ import {
 } from "firebase/auth";
 
 const Auth = () => {
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-	const [newAccount, setNewAccount] = useState(false);
-	const [error, setError] = useState("");
-	
-	const onChange = (event) => {
-		const {
-			target: { name, value },
-		} = event;
-		if (name === "email") {
-			setEmail(value);
-		} else if (name === "password") {
-			setPassword(value);
-		}
-	};
-	
-	const onSubmit = async (event) => {
-		event.preventDefault();
-		try {
-			let data;
-			if (newAccount) {
-				//create newAccount
-				data = await createUserWithEmailAndPassword(authService, email, password);
-			} else {
-				// log in
-				data =  await signInWithEmailAndPassword(authService, email, password);
-			}
-			console.log(data);
-		} catch (error) {
-			if(error.code === "auth/email-already-in-use"){
-				setError("이미 가입된 아이디니까 바꿔 ㅋ");
-				alert("fuck");
-			}
-			else {
-				setError(error.code);				
-			}
-		}
-	};
-
-			
-	const toggleAccount = () => setNewAccount((prev) => !prev);
 	
 	const onSocialClick = async (event) => {
 		const {
@@ -62,20 +21,11 @@ const Auth = () => {
 			provider = new GithubAuthProvider();
 		}
 		const data = await signInWithPopup(authService, provider);
-		console.log(data)
 	};
 	
 	return (
 	<div>
-		<form onSubmit={onSubmit}>
-			<input name="email" type="email" placeholder="Email" required value={email} onChange={onChange}/>
-			<input name="password" type="password" placeholder="Password" required value={password} onChange={onChange}/>
-			<input type="submit" value={newAccount ? "Create Account": "Log In"} />
-			{error}
-		</form>
-		<span onClick={toggleAccount}>
-			{newAccount ? "Sign In" : "Create Account"}
-		</span>	
+		<AuthForm />	
 		<div>
 			<button onClick={onSocialClick} name="google">
 				Continue with Google
